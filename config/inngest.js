c64f64900828564e30848f6ba45186bdda43a359
nguyentppse161945 <132319708+nguyentppse161945@users.noreply.github.com>
2025-08-21 07:38:15 +0700

    fix

---
+++ config/inngest.js
@@ -0,0 +1,58 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "shopify-next" });
+
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: "sync-user-creation"
+    },
+    { event: "clerk/user.created" },
+    async ({ event, }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name} ${last_name}`,
+            imageUrl: image_url,
+
+        }
+        await connectDB();
+        await User.create(userData);
+
+    }
+
+);
+//Inngest Function to update user data in db
+export const syncUserUpdate = inngest.createFunction(
+    {
+        id: "sync-user-update"
+    },
+    { event: "clerk/user.updated" },
+    async ({ event, }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name} ${last_name}`,
+            imageUrl: image_url,
+        }
+        await connectDB();
+        await User.findByIdAndUpdate(id, userData);
+    }
+);
+
+// Inngest Function to delete user data in db
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: "delete-user-deletion"
+    },
+    { event: "clerk/user.deleted" },
+    async ({ event, }) => {
+        const { id } = event.data;
+        await connectDB();
+        await User.findByIdAndDelete(id);
+    }
+);
