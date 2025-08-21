cc9853eb1fb393d3ebbb6fbef3b26618985260ef
nguyentppse161945 <132319708+nguyentppse161945@users.noreply.github.com>
2025-08-21 07:58:10 +0700

    fix2

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 let cached = global.mongoose;
 
-if(!catched){
+if(!cached){
     cached = global.mongoose = { conn: null, promise: null };
 }
 
--- config/inngest.js
+++ config/inngest.js
@@ -3,7 +3,7 @@ import connectDB from "./db";
 import User from "@/models/User";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "shopify-next" });
+export const inngest = new Inngest({ id: "shopify-next", telemetry: false,});
 
 export const syncUserCreation = inngest.createFunction(
     {
